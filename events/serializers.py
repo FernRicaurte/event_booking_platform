60ca5a154e49aa4e4da13c0797f619835439d4ed
FernRicaurte <FernRicaurte2@gmail.com>
2023-12-27 18:11:38 -0500

    adding docstrings for serilazers

--- events/serializers.py
+++ events/serializers.py
@@ -4,9 +4,25 @@ from .models import Event, Registration
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
-        
+        fields = '__all__'       
+"""
+Serializes the Event model for data representation.
+
+Attributes:
+    model (Event): The Event model to be serialized.
+    fields (list): The fields to be included in the serialized representation.
+
+"""      
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
         fields = '__all__'
+"""
+Serializes the Registration model for data representation.
+
+Attributes:
+    model (Registration): The Registration model to be serialized.
+    fields (list): The fields to be included in the serialized representation.
+
+"""
+

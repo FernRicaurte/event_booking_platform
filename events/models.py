5c1224a93bdc6441e3089647c6425f93ecd0543e
FernRicaurte <FernRicaurte2@gmail.com>
2023-12-27 01:27:44 -0500

    creating Event and Registration models/classes

---
+++ events/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    """
+Represents an event in the event booking platform.
+
+Args:
+    title (str): The title of the event.
+    description (str): The description of the event.
+    date (datetime.date): The date of the event.
+    time (datetime.time): The time of the event.
+    location (str): The location of the event.
+    organizer (User): The user who organized the event.
+
+Attributes:
+    title (str): The title of the event.
+    description (str): The description of the event.
+    date (datetime.date): The date of the event.
+    time (datetime.time): The time of the event.
+    location (str): The location of the event.
+    organizer (User): The user who organized the event.
+    created_at (datetime.datetime): The timestamp when the event was created.
+
+"""
+    def __str__(self):
+        return self.title
+    
+class Registration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attendee = models.ForeignKey(User, on_delete=models.CASCADE)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.attendee.username} - {self.event.title}"
+    
